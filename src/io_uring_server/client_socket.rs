8198dd2a9626e90d5b54fe634b418e6ee649eb5f
jean-eudes <jeudes7@gmail.com>
2025-07-06 21:03:33 +0200

    refactor client socket to remove unnecessary synchronization and simplify buffer management

--- src/io_uring_server/client_socket.rs
+++ src/io_uring_server/client_socket.rs
@@ -1,33 +1,35 @@
 use std::collections::HashMap;
 use std::os::fd::RawFd;
-use std::sync::{Arc, Mutex};
 
 pub struct Connection {
-    buffers: Arc<Mutex<HashMap<i32, Vec<u8>>>>
+    buffers: HashMap<i32, Vec<u8>>,
 }
 
 impl Connection {
     pub fn new() -> Self {
         Connection {
-            buffers: Arc::new(Mutex::new(HashMap::new())),
+            buffers: HashMap::new(),
         }
     }
 
     pub fn disconnect(&mut self, fd: RawFd) {
-        self.buffers.lock().unwrap().remove(&fd);
+        self.buffers.remove(&fd);
     }
 
-    pub fn get_buffer(&self, fd:i32) -> Vec<u8> {
-        self.buffers.lock().unwrap().get(&fd).unwrap().clone()
+    pub fn get_buffer(&self, fd: i32) -> Vec<u8> {
+        self.buffers.get(&fd).unwrap().clone()
     }
 
     pub fn accept_new_client(&mut self, fd: RawFd) {
-        self.buffers.lock().unwrap().insert(fd, vec![0u8; 512]);
+        self.buffers.insert(fd, vec![0u8; 512]);
     }
 
     pub fn receive_data(&mut self, fd: &RawFd, n: usize) {
-        let guard = self.buffers.lock().unwrap();
-        let buf = guard.get(fd).unwrap();
-        println!("Received {} from {}", String::from_utf8_lossy(&buf[..n]).replace('\n', ""), fd);
+        let buf = self.buffers.get(fd).unwrap();
+        println!(
+            "Received {} from {}",
+            String::from_utf8_lossy(&buf[..n]).replace('\n', ""),
+            fd
+        );
     }
 }
